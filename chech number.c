7ba3ecffd6a6e2b5926611a48d803a065e28bbf1
Divyavelu <divyachennai15@gmail.com>
2017-07-20 10:57:17 +0530

    chech number.c

---
+++ chech number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+{
+if(n>0)
+{
+printf("the given number is positive");
+}
+else if(n<0)
+{
+printf("the given number is negative");
+}
+else
+{
+printf("the number is zero");
+}
+}
+}
